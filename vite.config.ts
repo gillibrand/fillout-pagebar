479e91db22c3af8af8bdf6cb2fb9f96ac8c9df7f
Jay Gillibrand <jay.gillibrand@gmail.com>
2025-06-18 08:54:24 -0700

    Update slug for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fillout-pagebar/",
+
   plugins: [react(), tsconfigPaths(), svgr()],
 });
